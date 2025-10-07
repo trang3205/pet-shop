b654e4a17ce74290588eb24cdba2686755abd12d
Mai Trang <trangthuymai302@gmail.com>
2025-10-07 23:23:31 +0700

    Khởi chạy dự án với Node.js

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello, World!");
+});
+
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
+});
