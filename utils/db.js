d360cf7763a15e206acf4643dd148a4c1820e489
Mai Trang <trangthuymai302@gmail.com>
2025-10-29 23:23:24 +0700

    Fix: Connection to database.

--- utils/db.js
+++ utils/db.js
@@ -2,14 +2,7 @@ import knex from "knex";
 
 const db = knex({
   client: "pg",
-  connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    //ssl: { rejectUnauthorized: false }, // Sử dụng SSL nếu cần (khi deploy lên đám mây)
-  },
+  connection: process.env.DATABASE_URL + "?sslmode=no-verify",
   pool: { min: 0, max: 15 },
 });
 
